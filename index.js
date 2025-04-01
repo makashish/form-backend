95546f7887834ade74a1031de37dc7cd288979a9
ASHISH <makanindian@gmail.com>
2025-04-01 23:43:26 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+const JobSchema = new mongoose.Schema({ title: String, description: String, company: String });
+const Job = mongoose.model('Job', JobSchema);
+
+app.post('/jobs', async (req, res) => {
+  const job = new Job(req.body);
+  await job.save();
+  res.send(job);
+});
+
+app.get('/jobs', async (req, res) => {
+  res.send(await Job.find());
+});
+
+app.listen(5000, () => console.log('Server running on port 5000'));
