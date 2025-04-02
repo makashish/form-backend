502629c6fc7ce4a4c590c85290b28b22121e7bce
ASHISH <makanindian@gmail.com>
2025-04-02 22:13:02 +0530

    Updated features

--- index.js
+++ index.js
@@ -20,8 +20,12 @@ app.post('/jobs', async (req, res) => {
   res.send(job);
 });
 
+app.get('/jobs', async (req, res) => {
+  res.send(await Job.find());
+});
+
 app.get("/", (req, res) => {
-  res.send("server is running");
+  res.send("server is running!");
 });
 
 const PORT = process.env.PORT || 5000;
