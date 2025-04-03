7cb90e1b2876482d605b02f64ae4338983b5ebea
ASHISH <makanindian@gmail.com>
2025-04-03 11:16:51 +0530

    Updated features

--- index.js
+++ index.js
@@ -38,5 +38,5 @@ app.get('/jobs', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
